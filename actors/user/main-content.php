071217f504651726eebfa213776ce4a1838c6eee
rysntkang <132887121+rysntkang@users.noreply.github.com>
2024-02-16 13:59:28 +0800

    Delete actors directory

--- actors/user/main-content.php
+++
@@ -1,7 +0,0 @@
-<?php
-  if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-    include('pages/'. $page . '.php');
-  } else {
-    include('pages/viewAllParkingLocationPage.php'); // Load a default page if no page is specified
-  }
